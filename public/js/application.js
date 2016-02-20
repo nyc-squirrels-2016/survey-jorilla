aa4fa2aa23490a421c23ebde4fd5a5f334fda0b7
Joseph Kim & Joe Plonsker <pair+josephkim88.plonsker@devbootcamp.com>
2016-02-20 18:29:07 -0500

    commit at 6:30 pm for paired session

--- public/js/application.js
+++ public/js/application.js
@@ -5,32 +5,12 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
+  $(".add-choice").click(function(){
+    // var newInput = $(document.createElement("choice-field"))
+    // var newInput = $("choices[]");
+    $(".choice-field").clone(true).appendTo(".choice-input");
 
-
-
-
-  // $('.question-field-wrapper').each(function() {
-  //     var $question_wrapper = $('.question-fields', this);
-  //     $(".add-field", $(this)).click(function(e) {
-  //         $('.question-field:first-child', $question_wrapper).clone(true).appendTo($question_wrapper).find('input').val('').focus();
-  //     });
-  //     $('.question-field .remove-field', $question_wrapper).click(function() {
-  //         if ($('.question-field', $question_wrapper).length > 1)
-  //             $(this).parent('.question-field').remove();
-  //     });
-
-  //   });
-
-  // $('.choice-field-wrapper').each(function() {
-  //     var $choice_wrapper = $('.choice-fields', this);
-  //     $(".add-field", $(this)).click(function(e) {
-  //         $('.choice-field:first-child', $choice_wrapper).clone(true).appendTo($choice_wrapper).find('input').val('').focus();
-  //     });
-  //     $('.choice-field .remove-field', $choice_wrapper).click(function() {
-  //         if ($('.choice-field', $choice_wrapper).length > 1)
-  //             $(this).parent('.choice-field').remove();
-  //     });
-  // });
+  });
 
 
 

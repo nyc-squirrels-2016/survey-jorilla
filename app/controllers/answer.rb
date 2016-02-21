db9eb69f02a5541d92568308f7666e32b5161fc4
Lucas Nagle <lnagle@gmail.com>
2016-02-21 13:24:08 -0500

    Add working submits while taking a survey. MVP complete

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,8 +1,11 @@
 post '/answers' do
   @answer = Answer.new(choice_id: params[:answer])
-  @answer[:question_id] = @answer.choice.question.id
-  @answer[:user_id] = session[:user_id]
-  @answer[:survey_id] = @answer.question.survey.id
-  @answer.save
-  redirect '/'
+  if request.xhr?
+    @answer[:question_id] = @answer.choice.question.id
+    @answer[:user_id] = session[:user_id]
+    @answer[:survey_id] = @answer.question.survey.id
+    @answer.save
+  else
+    redirect '/'
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-   var choice =
-   // '<div class="choice-field">',
-   '<input class="choice-input" type="text" name="choice[]"><br>'
-   // '</div>'
+  var choice = '<input class="choice-input" type="text" name="choice[]"><br>'
 
-  $(".add-choice").click(function(event){
-    event.preventDefault();
-    // debugger;
-    // var newInput = $(document.createElement("choice-field"))
-    // var newInput = $("choices[]");
+  $(".add-choice").click(function(e){
+    e.preventDefault();
     $(".choice-field").append(choice);
-
-
   });
 
-
-
+  $(".individual-question").on('submit', function(e) {
+    e.preventDefault();
+    var count = 1
+    var request = $.ajax({
+      method: "POST",
+      url: e.target.action,
+      data: $(this).serialize()
+    });
+    request.done(function(){
+      $(e.target).hide();
+    });
+  });
 });
